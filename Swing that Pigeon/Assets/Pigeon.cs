ae8667eb5417fe047c915c28c20430240c98c9a5
JackBurleigh508 <33128868+JackBurleigh508@users.noreply.github.com>
2019-02-19 14:46:32 +0000

    The Pigeon is falling
    I repeat, the pigeon is falling

---
+++ Swing that Pigeon/Assets/Pigeon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pigeon : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.GetComponent<Rigidbody2D>().gravityScale = 0;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            this.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+    }
+}
