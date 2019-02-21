7f1c095c988e6cefbe5c8992079a40c9e85b7514
JackBurleigh508 <33128868+JackBurleigh508@users.noreply.github.com>
2019-02-21 16:27:45 +0000

    Now there's paaaint
    :O

--- Swing that Pigeon/Assets/Scripts/Paint.cs
+++ Swing that Pigeon/Assets/Scripts/Paint.cs
@@ -6,6 +6,10 @@ public class Paint : MonoBehaviour
 {
 
     private LineRenderer LR;
+
+    Vector2 objPos;
+    public int n = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,17 @@ public class Paint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        objPos = this.transform.position;
+
+        StartCoroutine(Painting());
+    }
+
+    IEnumerator Painting()
+    {
+        this.GetComponent<LineRenderer>().positionCount += 1;
+        this.GetComponent<LineRenderer>().SetPosition(n, objPos);
+        n += 1;
+        yield return new WaitForSeconds(0.001f);
+        StartCoroutine(Painting());
     }
 }
