fbd43988c90accd26d42856cc5f41324d0c1e77d
Ian Hughes <ianhughes.hoverbot@gmail.com>
2019-02-28 00:34:31 +0000

    Hyuuuuge Patch 1.18a
    - Imported new art (needs to be implemented)
    - Imported sounds (need to be implemented)
    - Imported and added music to scenes
    - New MusicManager script to keep things tidy
    - New Victory and Defeat scenes. Victory scene works but the game might need a fail condition (falling out of the level / off screen?)
    - Lil tweaks to clean things up

---
+++ Swing that Pigeon/Assets/Scripts/MusicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicController : MonoBehaviour
+{
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+
+    void Update()
+    {
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "MenuScreen" || scene.name == "Victory" || scene.name == "Defeat")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
